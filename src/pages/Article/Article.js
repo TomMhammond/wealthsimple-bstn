ef2491cbd10865f948f32be32f2579fefe9db52f
dani <118696363+dwleung@users.noreply.github.com>
2024-01-26 15:38:21 -0500

    Make popup

--- src/pages/Article/Article.js
+++ src/pages/Article/Article.js
@@ -1,38 +1,84 @@
 import PageHeader from "../../components/PageHeader/PageHeader";
-import piggyBank from "../../assets/images/piggy-bank-on-desk.png"
-import './Article.scss'
+import piggyBank from "../../assets/images/piggy-bank-on-desk.png";
+import "./Article.scss";
 import ArticleCard from "../../components/Cards/ArticleCard/ArticleCard";
+import { useState } from "react";
 
 export default function Article() {
+	const [isShowing, setisShowing] = useState(false);
+
+	const popupClickHandler = () => {
+		setisShowing(!isShowing);
+	};
+
 	return (
 		<>
 			<PageHeader title={"Learning Centre"} />
 			<main className="main">
-				<h1 className="article__title">Article Title</h1>
-				<h4 className="article__sub-title">Article Sub-title</h4>
-				<img className="article__img" src={piggyBank} alt="piggy bank on desk"></img>
-				<h4 className="article__sub-header">Article Sub header</h4>
+				<h1 className="article__title">What is a TFSA Account?</h1>
+				<img
+					className="article__img"
+					src={piggyBank}
+					alt="piggy bank on desk"
+				></img>
 				<p className="article__para">
-					Investing small amounts of money can pave the way to financial security through the magic of compound interest. Even modest contributions, when consistently invested over time, can yield impressive returns. The key is to start early and maintain a disciplined approach.
+					<span className="bold">
+						A perfect account if you have big savings goals in
+						a smaller timeframe. Withdraw anytime, tax-free.{" "}
+					</span>
 				</p>
 				<p className="article__para">
-					By embracing a long-term perspective and leveraging strategies like dollar-cost averaging, investors can navigate market fluctuations with confidence. Technology has made investing more accessible than ever, with platforms offering low fees and minimum requirements, enabling individuals to automate contributions and track their progress effortlessly.
+					A tax-free savings account is an account available in
+					Canada that provides tax benefits for saving.
+					Investment income, including{" "}
+					<span className="highlight">capital gains</span> and
+					<span className="highlight">dividends</span>, earned in
+					a TFSA is not taxed in most cases, even when withdrawn.
+					Contributions to a TFSA are not deductible for income
+					tax purposes, unlike contributions to a{" "}
+					<span
+						className="highlight"
+						onClick={popupClickHandler}
+					>
+						registered retirement savings plan
+					</span>
+					(RRSP).
 				</p>
-				<p className="article__para">
-					Diversification is crucial when investing small amounts, spreading risk across various asset classes to enhance returns and mitigate volatility. Regular portfolio reviews ensure alignment with changing financial goals and risk tolerances.
-				</p>
-				<p className="article__para">
-					In conclusion, investing small amounts is not only feasible but also a prudent strategy for building wealth over time. Whether it's through automated contributions or manual deposits, the key is consistency. With patience, discipline, and a focus on long-term growth, even the smallest investments can grow into significant assets.
-				</p>
-				
-			<aside>
 
-			<ArticleCard />
-			{/* carousel component should go here */}
+				<p className="article__para">
+					<span className="bold">Keep your growth tax-free</span>
+				</p>
+				<p className="article__para">
+					With a TFSA, you won’t pay taxes on any gains on your
+					investments — it’s yours to keep. Withdraw whenever you
+					need to Take money out of your TFSA any time,
+					hassle-free. The withdrawn amount is added back to your
+					contribution room the following year. Save without
+					restrictions Use your TFSA to save for a home,
+					education, or any big life expense. There are no limits
+					on how you use the funds.
+				</p>
 
-			</aside>
+				<aside>
+					<ArticleCard />
+					{/* carousel component should go here */}
+				</aside>
+				<div
+					className={`popup ${isShowing ? "" : "popup--hidden"}`}
+					onClick={popupClickHandler}
+				>
+					<p className="popup__title">
+						<span className="bold">
+							Registered Retirement Savings Plan
+						</span>
+					</p>
+					<p className="popup__content">
+						An RRSP (Registered Retirement Savings Plan) is a
+						savings plan that you can contribute to over the
+						course of your working life.
+					</p>
+				</div>
 			</main>
-
 		</>
 	);
 }
